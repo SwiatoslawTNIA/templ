d5900a468a1be56e38ca96f7a683dc63ab4b65d2
Swiatoslaw <swiatoslaw.p@gmail.com>
2024-07-09 19:05:12 +0200

    Created the working space

---
+++ first/sources/c++.cc
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------------------------------------------------
+// c++.cc
+//
+// <Explanation of the program ...>
+// <... May have multiple lines.>
+//
+//---------------------------------------------------------------------------------------------------------------------
+//
+#include "c++.h"
+//---------------------------------------------------------------------------------------------------------------------
+///
+/// This is an example header comment. Here, you explain, what the
+/// function does. Copypaste and adapt it!
+///
+/// @param numbers an array to check
+/// @param length The length of the array. If this text is too long you can
+///
+/// @return the highest number
+//
+int main(void)
+{
+  using namespace std;
+  cout << "Everything works alright.";
+  return 0;
+}
