22c30f902e5ab29064475f39aa2febc6fdc79ca6
Swiatoslaw <swiatoslaw@Swiatoslaws-MacBook-Air.local>
2024-07-10 23:28:32 +0200

    Started Chapter 3

--- first/sources/c++.cc
+++ first/sources/c++.cc
@@ -62,14 +62,57 @@ const char *max(const char * s1, const char *s2)//specific overloading for const
 {
   return strcmp(s1, s2) < 0? s1: s2;
 }
+//the larger size I specify, the more data can go unused, the less size I specify, 
+//the more likely, that the stack can get full
+template<typename T, std::size_t MaxSize>
+class Stack
+{
+  private:
+    std::array<T, MaxSize> elems;
+    std::size_t numElems;
+  public:
+    Stack();
+    void push(const T &ref) noexcept;
+    void pop(void) noexcept;
+    T const& top(void) const noexcept {return elems[0];}
+    bool empty(void) const noexcept {return elems.size();}
+    std::size_t size(void) const noexcept
+};
+template<typename T, std::size_t MaxSize>
+Stack<T,MaxSize>::Stack(): numElems(0){}
+//a default constructor template for tor two args
+template<typename T, std::size_t MaxSize>
+void Stack<T,MaxSize>::push(const T& ref) noexcept
+{
+  assert(numElems < MaxSize);// the number of the elements should be less than the MaxSize of the array
+  elems[numElems++] = ref;
+}
+template<typename T, std::size_t MaxSize>
+void Stack<T,MaxSize>::pop(void) noexcept
+{
+  assert(numElems > 0);
+  --numElems;//since it's an array
+}
+template<typename T, std::size_t MaxSize>
+const T& Stack<T,MaxSize>::top(void) const noexcept 
+{
+  assert(numElems > 0);
+  return elems[numElems - 1];
+}//to use the template, we have to specify both the param type and the arr_size:
+
+
 int main(void)
 {
   using namespace std;
-  int i = 7;
-  std::cout << "max (7, i) " << ::max(7, i) << std::endl;//int template will be used
-  double f1 = 3.4, f2 = -6.7;
-  std::cout << "max(f1, f2) " << ::max(f1, f2) << std::endl;//int template
-  std::string s1 = "math", s2 = std::string("mathematics");
-  std::cout << "max(s1, s2) " << ::max(s1, s2) << std::endl;//std::stirng template(1. one)
+  // int i = 7;
+  // std::cout << "max (7, i) " << ::max(7, i) << std::endl;//int template will be used
+  // double f1 = 3.4, f2 = -6.7;
+  // std::cout << "max(f1, f2) " << ::max(f1, f2) << std::endl;//int template
+  // std::string s1 = "math", s2 = std::string("mathematics");
+  // std::cout << "max(s1, s2) " << ::max(s1, s2) << std::endl;//std::stirng template(1. one)
+  Stack<int, 20> stack1;//stack of up to 20 ints
+  Stack<int, 40> stack2;//stack of up to 40 ints;
+  Stack<std::string, 40> strings;
+  
   return 0;
 }
